ba02749510e48be1f1f2db9b1989fe7a3a39659d
alejandrojaimerodri <alejandro.rodriguez.c1@ucb.edu.bo>
2024-09-17 09:30:30 -0400

    2024-09-17_01 iniciando código juego de la adivinanza

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+
+std::cout<<"**************************************"<<std::endl;
+
+std::cout<<"*Bienvenido al JUEGO de la adivinanza!*"<<std::endl;
+std::cout<<"***************************************"<<std::endl;
+
+}
