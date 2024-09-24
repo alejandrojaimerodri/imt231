08d5fe1cb40f64410b1838e1a764194fe34759ff
alejandrojaimerodri <alejandro.jaime.rodriguez.camacho@gmail.com>
2024-09-23 21:46:34 -0400

    juegoadivinanza 2024-09-23_03

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+int main(){
+
+cout << "************************" << endl;
+cout << "Bienvenido al juego de la adivinanza" << endl;
+cout << "************************" << endl;
+
+int numero_secreto = 42;
+cout <<"El número secreto es ..." << numero_secreto << ". No lodiga a para nadie! "  << endl;
+
+int adivina;
+cout << "Cuál es  el número" ;
+cin >> adivina;
+cout << "El valor de su número es: " << adivina << endl;
+
+}
