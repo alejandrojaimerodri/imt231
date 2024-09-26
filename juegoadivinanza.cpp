516f07293aa979464bfaeb30664c6e7078bd8f57
alejandrojaimerodri <alejandro.rodriguez.c1@ucb.edu.bo>
2024-09-26 11:25:17 -0400

    2024-09-24_01 genera numeroa aleatorios

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,17 +1,27 @@
 #include <iostream> 
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 int main(){
 
-cout << "************************" << endl;
-cout << "Bienvenido al juego de la adivinanza" << endl;
-cout << "************************" << endl;
+cout << "*****************************************" << endl;
+cout << "* Bienvenido al juego de la adivinanza * " << endl;
+cout << "* Ingrese un número en el rango: 1 a 100* " << endl;
+cout << "*****************************************" << endl;
 
-int NUMERO_SECRETO = 42;
+
+srand(time(0));
+const int NUMERO_SECRETO = 1 + rand() % 100;
+cout<< "Número secreto aleatorio generado entre 1 y 100: " << NUMERO_SECRETO << endl;
+
 int adivina;
 
 bool no_acerto = true;
 int intentos = 0;
 double puntos = 1000.0;  // ... guarda los puntos ganados
+
+
 
 while (no_acerto) {
 	intentos ++;
@@ -33,10 +43,13 @@ while (no_acerto) {
     		cout << "El número ingresado es mayor que el número secreto." << endl;
 	} 
 	else {
-
+	
+	
     	cout << "El número ingresado es menor que el número secreto." << endl;
 	}
 }
+
+
 cout << "Finalizo el juego" << endl;
 cout << "Adivino el numero secreto en " << intentos << " intentos." << endl;
 cout.precision(2);
